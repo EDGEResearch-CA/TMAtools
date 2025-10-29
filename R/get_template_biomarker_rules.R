005aa101f17bef56be49200632eabcb94ed3a0eb
biancarsbio <biancarsbio@gmail.com>
2025-10-28 22:06:38 -0700

    document template fun

--- R/get_template_biomarker_rules.R
+++ R/get_template_biomarker_rules.R
@@ -1,10 +1,13 @@
 #' Get template biomarker rules file
 #' @param output_file Path to output file. The template will be copied to this path.
 #' @export
-get_biomarker_rules_template <- function(output_file = "biomarker_rules_enoc.xlsx") {
-    template_path <- system.file(
-        "extdata", "biomarker_rules_enoc.xlsx",
-        package = "TMAtools"
-    )
-    file.copy(template_path, output_file)
+get_biomarker_rules_template <- function(
+  output_file = "biomarker_rules_template.xlsx"
+) {
+  template_path <- system.file(
+    "extdata",
+    "biomarker_rules_enoc.xlsx",
+    package = "TMAtools"
+  )
+  file.copy(template_path, output_file)
 }
